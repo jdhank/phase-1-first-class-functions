985236420517603bd150fedb4684fb95c7731543
Jake Hankins <jakehankins@comcast.net>
2022-04-07 23:10:59 -0400

    commit tree c795f33c551e2c7c2fb3ff97f5e441b2fe3793fb

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(cb){
+    cb()
+}
+function returnsANamedFunction(){
+    return function name (){}
+}
+function returnsAnAnonymousFunction(){
+    return function (){}
+}
